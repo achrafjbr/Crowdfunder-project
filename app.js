be4657149e535798e3ac61cf921ea6f23b903d4d
outeglaout jaouad <outeglaoutjaouad@gmail.com>
2026-03-30 12:11:50 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, (err)=>{if(!err)console.log('Start Server...')})
